mod: minor fix for numerical stability
add: direct feed beta and s into algo

algo_func.py
@@ -32,7 +32,7 @@ class Initializer:
         prob_mat = copy.deepcopy(prob_mat)
         # NOTE: adjusted here
         if prob_regularization:
-            prob_mat += 1.
+            prob_mat += 0.1
         n_size = prob_mat.shape[0]
         prob_mat = prob_mat.astype(np.double)
         for i in range(n_size):
@@ -79,8 +79,9 @@ class PopulationInitializer(Initializer):
 
         # ------------ use all judge info to calc
         mixed = p.sum(axis=0)
-        t = self.calculate_transition(mixed)
-        sp = np.log(self.calculate_stationary_distribution(t))
+        t = self.calculate_transition(mixed, self.config.prob_regularization)
+        w = self.calculate_stationary_distribution(t)
+        sp = np.log(np.abs(w))
         # normalize s for easy comparison
         sp -= np.min(sp)
         sp /= sp.sum()
@@ -151,7 +152,7 @@ class IndividualInitializer(Initializer):
             # u1 = np.matmul(np.matmul(np.linalg.gamma(np.matmul(A.T, A)), A.T), b)
             u = scipy.sparse.linalg.lsqr(a, b, show=self.verbose)[0]
 
-            q = np.hstack([np.log(u)])
+            q = np.hstack([np.log(np.abs(u))])
             qs.append(q)
 
         qs = np.vstack(qs)
@@ -306,19 +307,69 @@ class RankAggregation:
             # TODO: manual gradient
             raise NotImplementedError
 
-        if self.config.normalize_gradient:
+        if self.config.normalize_gradient and not self.config.linesearch:
             for pa_index, pa in enumerate(self.parameters):
                 grad_norm = torch.sqrt(torch.sum(pa.grad.data * pa.grad.data))
-                pa.grad.data /= grad_norm * 0.01
+                pa.grad.data /= grad_norm
 
-        self.optimizer.step()
-        self.sched.step(self.pr)
+        if self.config.linesearch:
+            a = 0.35
+            b = 0.8
+            t = 1.
+            s = self.parameters[1]
+            bb = self.parameters[0]
+
+            assert s.shape[0] == self.n_items
+            assert bb.shape[0] == self.n_judges
+
+            x = np.hstack([s.data, bb.data])
+            dx = np.hstack([s.grad.data, bb.grad.data])
+            if np.sqrt(np.sum(dx * dx)) < 10e-5:
+                return True
+
+            test_x = x - t * dx
+            test_s = test_x[:self.n_items]
+            test_bb = test_x[self.n_items:]
+
+            test_p = self.compute_likelihood_np(test_s, test_bb)
+            old_p = np.array(self.pr.data)
+            # old_p2 = self.compute_likelihood_np(self.s.detach().cpu().numpy(), self.beta.detach().cpu().numpy())
+            # print('starting search, initial p: ', old_p, 'test_p', test_p)
+
+            while test_p > old_p - a * t * dx.dot(dx):
+                t = b * t
+                test_x = x - t * dx
+                test_s = test_x[:self.n_items]
+                test_bb = test_x[self.n_items:]
+                # print('new step:', t, 'test_p: ', test_p)
+                # prev_p = test_p
+                test_p = self.compute_likelihood_np(test_s, test_bb)
+                # if (prev_p == test_p) / prev_p < 0.0001:
+                # if prev_p == test_p:
+                #     break
+
+            s.data *= 0.
+            s.data += torch.tensor(test_s, dtype=self.dtype, device=self.device)
+            bb.data *= 0.
+            bb.data += torch.tensor(test_bb, dtype=self.dtype, device=self.device)
+        else:
+            self.optimizer.step()
+
+        if not self.config.linesearch:
+            self.sched.step(self.pr)
 
         if not self.config.fix_s:
             self.post_process()
 
-        self.pr_list.append(self.pr.detach().cpu().numpy())
+        num_pr = self.pr.detach().cpu().numpy().tolist()
+        assert not isinstance(num_pr, list)
+        self.pr_list.append(num_pr)
         self.s_list.append(np.linalg.norm(self.s.data.cpu().numpy() - self.s_true))
+
+        # if self.config.linesearch and len(self.pr_list) > 10 and abs(self.pr_list[-10] - self.pr_list[-1]) / self.pr_list[-10] < 0.001:
+        #     return True
+        # else:
+        return False
 
     def post_process(self):
         raise NotImplementedError
@@ -350,6 +401,14 @@ class BTLNaive(RankAggregation):
 
         pr = - torch.sum(torch.sum(self.count_mat, dim=0) * torch.log(torch.exp(si_minus_sj) + 1))
         self.pr = -pr / self.n_pairs
+
+    def compute_likelihood_np(self, s, beta):
+        sr_j = self.replicator.cpu().numpy() * s  # each column is the same value
+        sr_i = sr_j.T
+        si_minus_sj = sr_i - sr_j
+
+        pr = - np.sum(np.sum(self.count_mat.cpu().numpy(), dim=0) * np.log(np.exp(si_minus_sj) + 1))
+        return -pr / self.n_pairs
 
     def post_process(self):
         self.s.data -= torch.min(self.s.data)
@@ -399,12 +458,27 @@ class GBTLGamma(GBTL):
         ex = si_minus_sj.view((1,) + si_minus_sj.shape)
         iv = self.gamma.view(self.gamma.shape + (1, 1))
         qu = ex * iv
-        mask = (qu > 11).double()
+        mask = (qu > 23).double()
         q_approx = mask * qu
         q_exact = qu - q_approx
         lg = torch.log(torch.exp(q_exact) + 1) + q_approx
         pr = - torch.sum(self.count_mat * lg)
         self.pr = -pr / self.n_pairs
+
+    def compute_likelihood_np(self, s, gamma):
+        sr_j = self.replicator.cpu().numpy() * s  # each column is the same value
+        sr_i = sr_j.T
+        si_minus_sj = sr_i - sr_j
+
+        ex = si_minus_sj.reshape((1,) + si_minus_sj.shape)
+        iv = gamma.reshape(self.gamma.shape + (1, 1))
+        qu = ex * iv
+        mask = (qu > 23)
+        q_approx = mask * qu
+        q_exact = qu - q_approx
+        lg = np.log(np.exp(q_exact) + 1) + q_approx
+        pr = - np.sum(self.count_mat.cpu().numpy() * lg)
+        return -pr / self.n_pairs
 
     def post_process(self):
         self.s.data -= torch.min(self.s.data)
@@ -442,12 +516,27 @@ class GBTLBeta(GBTL):
         ex = si_minus_sj.view((1,) + si_minus_sj.shape)
         ep = self.beta.view(self.beta.shape + (1, 1))
         qu = ex / ep
-        mask = (qu > 11).double()
+        mask = (qu > 23).double()
         q_approx = mask * qu
         q_exact = qu - q_approx
         lg = torch.log(torch.exp(q_exact) + 1) + q_approx
         pr = - torch.sum(self.count_mat * lg)
         self.pr = -pr / self.n_pairs
+
+    def compute_likelihood_np(self, s, beta):
+        sr_j = self.replicator.cpu().numpy() * s  # each column is the same value
+        sr_i = sr_j.T
+        si_minus_sj = sr_i - sr_j
+
+        ex = si_minus_sj.reshape((1,) + si_minus_sj.shape)
+        ep = beta.reshape(beta.shape + (1, 1))
+        qu = ex / ep
+        mask = (qu > 23)
+        q_approx = mask * qu
+        q_exact = qu - q_approx
+        lg = np.log(np.exp(q_exact) + 1) + q_approx
+        pr = - np.sum(self.count_mat.cpu().numpy() * lg)
+        return -pr / self.n_pairs
 
     # def compute_gradient(self):
     #     grad_b = torch.sum(torch.sum(self.count_mat * (-1. / ep / ep * ex / (torch.exp(-qu) + 1)), dim=-1), dim=-1) / n_pairs
@@ -484,12 +573,27 @@ class GBTLEpsilon(GBTL):
         ex = si_minus_sj.view((1,) + si_minus_sj.shape)
         ep = (self.eps * self.eps).view(self.eps.shape + (1, 1))
         qu = ex / ep
-        mask = (qu > 11).double()
+        mask = (qu > 23).double()
         q_approx = mask * qu
         q_exact = qu - q_approx
         lg = torch.log(torch.exp(q_exact) + 1) + q_approx
         pr = - torch.sum(self.count_mat * lg)
         self.pr = -pr / self.n_pairs
+
+    def compute_likelihood_np(self, s, eps):
+        sr_j = self.replicator.cpu().numpy() * s  # each column is the same value
+        sr_i = sr_j.T
+        si_minus_sj = sr_i - sr_j
+
+        ex = si_minus_sj.reshape((1,) + si_minus_sj.shape)
+        ep = (eps * eps).reshape(eps.shape + (1, 1))
+        qu = ex / ep
+        mask = (qu > 23)
+        q_approx = mask * qu
+        q_exact = qu - q_approx
+        lg = np.log(np.exp(q_exact) + 1) + q_approx
+        pr = -np.sum(self.count_mat.cpu().numpy() * lg)
+        return -pr / self.n_pairs
 
     def post_process(self):
         self.s.data -= torch.min(self.s.data)
@@ -516,11 +620,15 @@ def make_estimation(data_pack, config):
         raise NotImplementedError
 
     algorithm.initialize()
-    algorithm.setup_optimizer()
+    require_opt = algorithm.setup_optimizer()
+    algorithm.init_print()
 
-    for i in range(config.max_iter):
-        algorithm.compute_likelihood()
-        algorithm.optimization_step()
+    if require_opt:
+        for i in range(config.max_iter):
+            algorithm.compute_likelihood()
+            if algorithm.optimization_step():
+                print('line search stop condition met -------')
+                break
 
     beta_est = algorithm.consolidate_result()
     s_est = algorithm.s.data.cpu().numpy()
@@ -538,7 +646,14 @@ def make_estimation(data_pack, config):
     # print(res_pack)
     res_pack.data_pack = data_pack
 
-    res_pack.pr_list = algorithm.pr_list[10:]
-    res_pack.s_list = algorithm.s_list[10:]
+    res_pack.pr_list = algorithm.pr_list[0:]
+    res_pack.s_list = algorithm.s_list[0:]
 
+    if config.linesearch:
+        pr_name = 'line'
+    else:
+        pr_name = 'sgd'
+    if config.init_method == 'disturb':
+        pr_name = 'gt'
+    res_pack.pr_name = pr_name
     return res_pack

data_helper.py
@@ -12,53 +12,60 @@ def generate_data(data_seed=None,
                   n_items=10, n_judges=10, n_pairs=200,
                   shrink_b=30, beta_gen_func='shrink', s_gen_func='spacing',
                   visualization=False, save_path=None,
-                  gen_by_pair=False, known_pairs_ratio=0.1, repeated_comps=32):
+                  gen_by_pair=False, known_pairs_ratio=0.1, repeated_comps=32,
+                  beta_true=None, s_true=None):
 
     if not data_seed:
         data_seed = int(time.time() * 10e7) % 2**32
     np.random.seed(data_seed)
-    
-    if s_gen_func == 'random':
-        s_true = np.sort(np.random.normal(loc=1.0, size=n_items))
-    elif s_gen_func == 'spacing':
-        po = np.arange(1., 2 * n_items + 1, 2.) - n_items
-        po = po / 2 / n_items
-        s_true = np.log(np.power(10, po))
-        print('ground truth s', s_true)
 
-    if beta_gen_func == 'manual':
-        assert len(shrink_b) == n_judges
-        beta_true = np.array(shrink_b)
-    if beta_gen_func == 'shrink':
-        beta_true = np.random.random(size=n_judges) / shrink_b
-    if beta_gen_func == 'ex':
-        beta_true = np.random.exponential(shrink_b, size=n_judges)
-    elif beta_gen_func == 'power':
-        beta_true = np.power(shrink_b, -1. * np.arange(0, n_judges))
-    elif beta_gen_func == 'xi':
-        assert shrink_b < len(beta_true)
-        beta_true = [0.00001, 0.0001, 0.0002, 0.0005,
-                 0.001, 0.005,
-                 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-                 0.1, 0.5, 1.0]
-        beta_true = np.ones(n_judges) * beta_true[shrink_b]
-    elif beta_gen_func == 'beta':
-        beta_true = np.random.beta(shrink_b[0], shrink_b[1], size=n_judges)
-    elif beta_gen_func == 'gamma':
-        beta_true = np.random.gamma(shrink_b[0], shrink_b[1], size=n_judges)
-    elif beta_gen_func == 'negative':
-        n_positive = min(int(np.ceil(n_judges * 0.7)), n_judges - 1)
-        n_negative = n_judges - n_positive
-        beta_true = np.array([shrink_b] * n_positive + [-shrink_b] * n_negative)
+    if beta_true is not None and s_true is not None:
+        pass
+    else:
+        if s_gen_func == 'random':
+            s_true = np.sort(np.random.normal(loc=1.0, size=n_items))
+        elif s_gen_func == 'spacing':
+            po = np.arange(1., 2 * n_items + 1, 2.) - n_items
+            po = po / 2 / n_items
+            s_true = np.log(np.power(10, po))
+
+        if beta_gen_func == 'manual':
+            assert len(shrink_b) == n_judges
+            beta_true = np.array(shrink_b)
+        if beta_gen_func == 'shrink':
+            beta_true = np.random.random(size=n_judges) / shrink_b
+        if beta_gen_func == 'ex':
+            beta_true = np.random.exponential(shrink_b, size=n_judges)
+        elif beta_gen_func == 'power':
+            beta_true = np.power(shrink_b, -1. * np.arange(0, n_judges))
+        elif beta_gen_func == 'xi':
+            assert shrink_b < len(beta_true)
+            beta_true = [0.00001, 0.0001, 0.0002, 0.0005,
+                     0.001, 0.005,
+                     0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                     0.1, 0.5, 1.0]
+            beta_true = np.ones(n_judges) * beta_true[shrink_b]
+        elif beta_gen_func == 'beta':
+            beta_true = np.random.beta(shrink_b[0], shrink_b[1], size=n_judges)
+        elif beta_gen_func == 'gamma':
+            beta_true = np.random.gamma(shrink_b[0], shrink_b[1], size=n_judges)
+        elif beta_gen_func == 'negative':
+            n_positive = min(int(np.ceil(n_judges * 0.7)), n_judges - 1)
+            n_negative = n_judges - n_positive
+            beta_true = np.array([shrink_b] * n_positive + [-shrink_b] * n_negative)
+
+    print('ground truth s', s_true)
     print('ground truth beta', beta_true)
+    s_true = np.array(s_true)
+    beta_true = np.array(beta_true)
 
     # s_true = np.array([-0.3, 0.7, 0.2, 0.5, 0.1])
     # beta_true = np.array([0.1, 2.0, 0.05, 4.0, 1.0])
 
-    beta_idx = np.argsort(np.abs(beta_true))
-    ii = beta_idx[n_judges // 2]
-    beta_true[ii], beta_true[0] = beta_true[0], beta_true[ii]
-    # TODO: move this part to estimation rather than generation
+    # beta_idx = np.argsort(np.abs(beta_true))
+    # ii = beta_idx[n_judges // 2]
+    # beta_true[ii], beta_true[0] = beta_true[0], beta_true[ii]
+    # # TODO: move this part to estimation rather than generation
 
     s_true -= s_true[0]
     # s_true /= s_true.sum()

eval_paint.py
@@ -14,7 +14,8 @@ def err_func(pred):
 def acc_func(pred, src=None):
     if src is None:
         src = np.arange(len(pred))
-    return scipy.stats.spearmanr(pred, src)[0]
+    # return scipy.stats.spearmanr(pred, src)[0]
+    return scipy.stats.kendalltau(pred, src)[0]
 
 
 def get_eval(all_pack):
@@ -28,7 +29,7 @@ def get_eval(all_pack):
     return acc
 
 
-def gen_data(data_name, seed, save_path=None):
+def gen_data(data_name, seed, save_path=None, s_true=None, beta_true=None):
     dn = data_name.split('-')
 
     dn1_split = list(map(float, dn[1][1:].split(',')))
@@ -69,7 +70,9 @@ def gen_data(data_name, seed, save_path=None):
         'save_path': save_path,
         'known_pairs_ratio': known_pairs_ratio,
         'repeated_comps': repeated_comps,
-        'gen_by_pair': gen_by_pair
+        'gen_by_pair': gen_by_pair,
+        's_true': s_true,
+        'beta_true': beta_true,
     }
     data_pack = generate_data(**data_kwarg)
     return data_pack, data_kwarg
@@ -119,7 +122,7 @@ def run_algo(data_pack, data_kwarg, algo_name, seed):
     config.normalize_gradient = True
     config.GPU = True
     config.linesearch = False
-    config.prob_regularization = False
+    config.prob_regularization = True
     config.err_const = 10e-23
     res_pack = make_estimation(data_pack, config)
 

main.py
@@ -29,23 +29,72 @@ if __name__ == '__main__':
         exit(-1)
 
     data_name_suffix = [
+        '0.2k1',
+        '0.4k1',
         '0.8k1',
-        '1.0k1',
-        '0.16k5',
-        '0.08k10',
-        '0.04k20',
-        '0.8k4',
-        '1.0k4',
-        '0.16k20',
-        '0.08k40',
-        '0.04k80',
+        # '1.0k1',
+        # '0.16k5',
+        # '0.08k10',
+        # '0.04k20',
+        # '0.8k4',
+        # '1.0k4',
+        # '0.16k20',
+        # '0.08k40',
+        # '0.04k80',
+        # 'p10',
+        # 'p20',
+        # 'p30',
+        # 'p40',
+        # 'p50',
+        # 'p60',
+        # 'p70',
+        # 'p80',
+        # 'p90',
+        # 'p100',
+        # 'p200',
+        # 'p300',
+        # 'p400',
+        # 'p500',
+        # 'p600',
+        # 'p700',
+        # 'p800',
+        # 'p900',
+        # 'p1000',
+        # 'p2000',
+        # 'p3000',
+        # 'p4000',
+        # 'p5000',
+        # 'p6000',
+        # 'p7000',
+        # 'p8000',
+        # 'p9000',
+        # 'p10000',
+        # 'p20000',
+        # 'p30000',
+        # 'p40000',
+        # 'p50000',
+        # 'p60000',
+        # 'p70000',
+        # 'p80000',
+        # 'p90000',
+        # 'p100000',
+        # 'p200000',
+        # 'p300000',
+        # 'p400000',
+        # 'p500000',
+        # 'p600000',
+        # 'p700000',
+        # 'p800000',
+        # 'p900000',
+        # 'p2000000'
     ]
 
     algo_names = [
-        'btl-spectral-do',
+        # 'btl-spectral-do',
         # 'btl-spectral-mle',
         # 'btl-random-mle',
-        'gbtl-spectral_all-do',
+        # 'gbtlneg-spectral_all-do',
+        # 'gbtl-spectral_all-do',
     ]
     algo = [
         'gbtl-spectral_all-mle',
@@ -54,7 +103,7 @@ if __name__ == '__main__':
         'gbtlneg-random_all-mle',  #
         'gbtlinv-spectral_all-mle',  #
         'gbtlinv-random_all-mle',  #
-
+        #
         # 'gbtl-disturb-mle',
         # 'gbtlneg-disturb-mle',  #
         # 'gbtlinv-disturb-mle',  #
@@ -64,7 +113,31 @@ if __name__ == '__main__':
         # 'gbtlinv-disturb_random_b_fix^s-mle',  #
     ]
 
+    color = [
+        'red',
+        'green',
+        'blue'
+    ]
+    paint_pairs = [
+        [
+            'gbtl-spectral_all-mle',
+            'gbtl-disturb-mle',
+            'gbtl-random_all-mle',
+        ],
+        [
+            'gbtlneg-spectral_all-mle',
+            'gbtlneg-disturb-mle',
+            'gbtlneg-random_all-mle',
+        ],
+        [
+            'gbtlinv-spectral_all-mle',
+            'gbtlinv-disturb-mle',
+            'gbtlinv-random_all-mle',
+        ],
+    ]
+
     acc = Dict()
+    err = Dict()
     # key: dn + an
     # val: [result]
 
@@ -72,7 +145,8 @@ if __name__ == '__main__':
         for al in algo:
             algo_names.append(al+'-'+str(lr))
 
-    data_seeds = [1313, 3838, 6262, 1338, 1362, 3862, 6238, 6213, 3813, 13, 38, 62][3:4]
+    # keep 2:3
+    data_seeds = [1313, 3838, 6262, 1338, 1362, 3862, 6238, 6213, 3813, 13, 38, 62][2:3]
     start_t = time.time()
 
     if eval:
@@ -80,7 +154,7 @@ if __name__ == '__main__':
         f = open(os.path.join(base_str, base_str + '.txt'), 'r')
         data_name_bases = f.read().split(' ')
 
-        for algo_name in algo_names[:2]:
+        for algo_name in algo_names[:]:
             res_table = []
             for data_name_base in data_name_bases:
                 base_dir = base_str + '_' + data_name_base
@@ -90,7 +164,8 @@ if __name__ == '__main__':
                 for suffix in data_name_suffix:
                     st = data_name_base + '-' + suffix + '+' + algo_name
                     print(st)
-                    this_row.append(np.sum(res[st]) / len(res[st]))
+                    # this_row.append(np.sum(np.abs(res[st])) / len(res[st]))
+                    this_row.append(res[st][0])
                 res_table.append(this_row)
             df = pd.DataFrame(res_table, columns=cols)
             df.to_csv(os.path.join(base_str, algo_name+'.csv'))
@@ -114,11 +189,11 @@ if __name__ == '__main__':
         #                 ax1.plot(range(len(sgd[:25])), sgd[:25], '--', color=color[idx], label='sgd '+algo_name)
         #                 ax0.plot(range(len(line)), line, color=color[idx], label='line '+algo_name)
         #                 ax1.plot(range(len(line[:25])), line[:25], color=color[idx], label='line '+algo_name)
-        #
+        #             #
         #             ax1.legend(loc='upper right', fontsize='x-small')
         #             plt.savefig(os.path.join
         #                         (base_dir,
-        #                          'summary.' + data_name_base + '-' + suffix + '.{}.png'.format(algo_name.split('-')[0])),
+        #                          'summary+random.' + data_name_base + '-' + suffix + '.{}.png'.format(algo_name.split('-')[0])),
         #                         bbox_inches='tight', dpi=96)
         #             plt.close('all')
         exit(0)
@@ -141,27 +216,33 @@ if __name__ == '__main__':
                 if st not in acc:
                     acc[st] = []
 
+                if st not in err:
+                    err[st] = []
+
                 try:
                     all_pack = run_algo(data_pack, data_kwarg, algo_name, seed)
                     all_pack = Dict(all_pack)
 
-                    plt.figure(figsize=(6, 4)).suptitle('likelihood')
-                    plt.plot(all_pack.pr_list)
-                    plt.savefig(os.path.join(base_dir, st+'.pr_list.png'), bbox_inches='tight', dpi=96)
-                    plt.close()
+                    # plt.figure(figsize=(6, 4)).suptitle('likelihood')
+                    # plt.plot(all_pack.pr_list)
+                    # plt.savefig(os.path.join(base_dir,
+                    #                          st+'.pr_list.{}.png'.format(all_pack.pr_name)),
+                    #             bbox_inches='tight', dpi=96)
+                    open(os.path.join(base_dir
+                                      , st+'.pr_list.{}.json'.format(all_pack.pr_name)), 'w'
+                                ).write(json.dumps(all_pack.pr_list))
+                    # plt.close()
 
-                    plt.figure(figsize=(6, 4)).suptitle('s')
-                    plt.plot(all_pack.s_list)
-                    plt.savefig(os.path.join(base_dir, st + '.s_list.png'), bbox_inches='tight', dpi=96)
-                    plt.close()
+                    # plt.figure(figsize=(6, 4)).suptitle('s')
+                    # plt.plot(all_pack.s_list)
+                    # plt.savefig(os.path.join(base_dir, st + '.s_list.png'), bbox_inches='tight', dpi=96)
+                    # plt.close()
 
                     score = get_eval(all_pack)
                     acc[st].append(score)
-                    if (len(all_pack.pr_list) < 1):
-                        impp = 0.
-                    else:
-                        impp = (all_pack.pr_list[0] - all_pack.pr_list[-1]) / all_pack.pr_list[-1]
-                    imp[st].append(impp)
+                    err[st].append(
+                        {'s_err': np.linalg.norm(all_pack.s_est - all_pack.data_pack.s_true),
+                         'beta_err': np.linalg.norm((all_pack.beta_est - all_pack.data_pack.beta_true))/all_pack.beta_est})
                     print(st, score, '\n--------------------\n')
                 except np.linalg.linalg.LinAlgError:
                     print("Cannot solve equation for initialization.")
@@ -173,6 +254,6 @@ if __name__ == '__main__':
                       (time.time() - start_t))
 
     pprint.pprint(acc)
-    pprint.pprint(imp)
-    acc['imp'] = imp
+    pprint.pprint(err)
+    acc['err'] = err
     open(os.path.join(base_dir, data_name_base+'.json'), 'w').write(json.dumps(acc, indent=4))